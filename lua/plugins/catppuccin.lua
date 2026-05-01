91edb37ea3c5efdd3bc100f4276821e8a17857cc
almostSouji <timoqueezle@gmail.com>
2026-05-01 20:32:38 +0200

    add markdown plugin

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,12 +1,10 @@
 -- Soothing pastel theme for the high-spirited!
 
 return {
-  'catppuccin/nvim',
-  name = 'catppuccin',
+  "catppuccin/nvim",
+  name = "catppuccin",
   priority = 1000,
-  config = function()
-    vim.cmd.colorscheme 'catppuccin-mocha'
-  end,
+  config = function() vim.cmd.colorscheme "catppuccin-mocha" end,
 }
 
 -- vim: ts=2 sts=2 sw=2 et
