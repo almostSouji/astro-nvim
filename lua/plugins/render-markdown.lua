c2322494fba5c3e72e4b40518abe93e369d42d42
almostSouji <timoqueezle@gmail.com>
2026-05-29 22:44:43 +0200

    feat: nabla for md rendering

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -5,5 +5,12 @@ return {
   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
-  opts = {},
+  opts = {
+    latex = { enabled = false },
+    win_options = { conceallevel = { rendered = 2 } },
+    on = {
+      render = function() require("nabla").enable_virt { autogen = true } end,
+      clear = function() require("nabla").disable_virt() end,
+    },
+  },
 }
