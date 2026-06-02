54d277b88d634b2d971b2ec2847c1bd768279a68
almostSouji <timoqueezle@gmail.com>
2026-06-02 11:36:08 +0200

    fix: do not set conceal level on render markdown

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -7,7 +7,6 @@ return {
   ---@type render.md.UserConfig
   opts = {
     latex = { enabled = false },
-    win_options = { conceallevel = { rendered = 2 } },
     on = {
       render = function() require("nabla").enable_virt { autogen = true } end,
       clear = function() require("nabla").disable_virt() end,
